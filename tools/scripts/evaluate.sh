75c65672ca30f8a33050b07742d92abfc3cfe299
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-30 14:47:57 +0800

    add

--- tools/scripts/evaluate.sh
+++ tools/scripts/evaluate.sh
@@ -6,6 +6,8 @@ model_path="/data/yubo/models/Qwen2.5-Math-7B-CFT"
 output_dir="../evaluation_output"
 summary_path="../evaluation_summary/summary.txt"
 
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
 cd ../evaluate_math/scripts
 bash evaluate_qwen.sh ${model_path} ${output_dir} ${summary_path}
 
