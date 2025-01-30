9e483e578eb6a66fe991d216d61cd3990af6fcca
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-30 23:32:52 +0800

    add

--- train/Validation/test_validation.sh
+++ train/Validation/test_validation.sh
@@ -2,5 +2,5 @@
 model_dir="/data/yubo/models/Qwen2.5-Math-7B-CFT"
 summary_path="../../Validation/validation_summary.txt"
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 bash validate_single.sh $model_dir $summary_path
