c394551858e26a84baf8db574d773f3909371e14
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-04 13:14:14 +0800

    update construct critique data scripts

--- tools/self_construct_critique_data/run.sh
+++ tools/self_construct_critique_data/run.sh
@@ -1,4 +1,4 @@
 
 export OPENAI_API_KEY=YOUR_API_KEY
 
-bash generate_critique_by_api.py --model_name "gpt-4o-2024-11-20" --num_processes 20
+python generate_critique_by_api.py --model_name "gpt-4o-2024-11-20" --num_processes 20
